fb5db91b80bf932ce9dfe6b7100ef68a4b26052e
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-19 23:21:01 -0500

    FullstackOpen part8 section a exercice 8.2

--- index.js
+++ index.js
@@ -94,9 +94,17 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
@@ -104,6 +112,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
